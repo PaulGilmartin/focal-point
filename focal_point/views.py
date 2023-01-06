54d117ef61cfc0e9608ad41c6f51120ac42ed1b2
Paul Gilmartin <paul.gilmartin89@gmail.com>
2023-01-06 17:51:20 +0100

    Remove file after we generate the response

--- focal_point/views.py
+++ focal_point/views.py
@@ -1,4 +1,6 @@
-from django.http import FileResponse, HttpResponse
+import os
+
+from django.http import FileResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from focal_point.focal_point import focal_point
@@ -6,9 +8,9 @@ from focal_point.focal_point import focal_point
 
 @csrf_exempt
 def upload_file(request):
-    if request.method == 'GET':
-        return HttpResponse('Hey')
     if request.method == 'POST':
         file = request.FILES['file']
         edited_file_path = focal_point(file)
-        return FileResponse(open(edited_file_path, 'rb'))
+        response = FileResponse(open(edited_file_path, 'rb'))
+        os.remove(edited_file_path)
+        return response
